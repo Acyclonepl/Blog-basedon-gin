6957d5983eb702d79effee2dedd76d01e5db2264
Acyclonepl <570734320@qq.com>
2026-06-24 10:19:28 +0800

    2nd commit

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -9,3 +9,17 @@ type Tag struct {
 func (t Tag) TableName() string {
 	return "blog_tag"
 }
+
+// CreateTagRequest 创建标签请求
+type CreateTagRequest struct {
+	Name      string `json:"name" binding:"required,min=3,max=100" example:"Go"`
+	State     int    `json:"state" binding:"oneof=0 1" example:"1"`
+	CreatedBy string `json:"created_by" binding:"required,min=3,max=100" example:"admin"`
+}
+
+// UpdateTagRequest 更新标签请求
+type UpdateTagRequest struct {
+	Name       string `json:"name" binding:"min=3,max=100" example:"Golang"`
+	State      int    `json:"state" binding:"oneof=0 1" example:"1"`
+	ModifiedBy string `json:"modified_by" binding:"required,min=3,max=100" example:"admin"`
+}
--- internal/routers/api/v1/article.go
+++ internal/routers/api/v1/article.go
@@ -7,8 +7,54 @@ type Article struct{}
 func NewArticle() Article {
 	return Article{}
 }
-func (a Article) Get(c *gin.Context)    {}
-func (a Article) List(c *gin.Context)   {}
+
+// @Summary 获取单篇文章
+// @Produce json
+// @Param id path int true "文章ID"
+// @Success 200 {object} model.ArticleSwagger "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/articles/{id} [get]
+func (a Article) Get(c *gin.Context) {}
+
+// @Summary 获取文章列表
+// @Produce json
+// @Param title query string false "文章标题" maxlength(100)
+// @Param state query int false "状态" Enums(0,1) default(1)
+// @Param page query int false "页码"
+// @Param page_size query int false "每页数量"
+// @Success 200 {object} model.ArticleSwagger "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/articles [get]
+func (a Article) List(c *gin.Context) {}
+
+// @Summary 新增文章
+// @Produce json
+// @Accept json
+// @Param request body model.CreateArticleRequest true "创建文章请求"
+// @Success 200 {object} model.ArticleSwagger "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/articles [post]
 func (a Article) Create(c *gin.Context) {}
+
+// @Summary 更新文章
+// @Produce json
+// @Accept json
+// @Param id path int true "文章ID"
+// @Param request body model.UpdateArticleRequest true "更新文章请求"
+// @Success 200 {object} model.ArticleSwagger "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/articles/{id} [put]
 func (a Article) Update(c *gin.Context) {}
+
+// @Summary 删除文章
+// @Produce json
+// @Param id path int true "文章ID"
+// @Success 200 {string} string "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/articles/{id} [delete]
 func (a Article) Delete(c *gin.Context) {}
